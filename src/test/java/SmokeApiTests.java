897b5a33d48f1edccd2161abb27e4f8bd2e13d2e
Sensation3000 <omsksmp@gmail.com>
2025-04-13 02:43:29 +0300

    add controller

--- src/test/java/SmokeApiTests.java
+++ src/test/java/SmokeApiTests.java
@@ -1,36 +1,60 @@
+import controllers.UserController;
 import io.restassured.response.Response;
+import models.AddUserResponse;
+import models.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 
 public class SmokeApiTests {
+    UserController userController = new UserController();
 
     @Test
     void simpleTest() {
         String body = """
-           {
-             "id": 0,
-             "username": "string",
-             "firstName": "string",
-             "lastName": "string",
-             "email": "string",
-             "password": "string",
-             "phone": "string",
-             "userStatus": 0
-           }""";
+                {
+                  "id": 0,
+                  "username": "string",
+                  "firstName": "string",
+                  "lastName": "string",
+                  "email": "string",
+                  "password": "string",
+                  "phone": "string",
+                  "userStatus": 0
+                }""";
 
 
         Response response = given()
-                  .header("accept", "application/json")
-                  .header("Content-Type", "application/json")
-                  .baseUri("https://petstore.swagger.io/v2/")
+                .header("accept", "application/json")
+                .header("Content-Type", "application/json")
+                .baseUri("https://petstore.swagger.io/v2/")
                 .when()
-                  .body(body)
-                  .post("user")
+                .body(body)
+                .post("user")
                 .andReturn();
         int actualCode = response.getStatusCode();
 
         Assertions.assertEquals(200, actualCode);
+    }
+
+    @Test
+    void simpleTest2() {
+        User user = User.builder()
+                .username("username")
+                .firstName("firstName")
+                .lastName("lastName")
+                .email("email")
+                .phone("password")
+                .userStatus(0)
+                .build();
+
+        Response response = userController.createUser(user);
+        AddUserResponse createdUserResponse = response.as(AddUserResponse.class);
+
+        Assertions.assertEquals(200, response.statusCode());
+        Assertions.assertEquals(200, createdUserResponse.getCode());
+        Assertions.assertEquals("unknown", createdUserResponse.getType());
+        Assertions.assertFalse(createdUserResponse.getMessage().isEmpty());
     }
 }
