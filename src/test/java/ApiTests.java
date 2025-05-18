711679964e4c27747130a81685e81b9a64ca3cd6
Sensation3000 <omsksmp@gmail.com>
2025-05-18 17:31:07 +0300

    add constants

--- src/test/java/ApiTests.java
+++ src/test/java/ApiTests.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
+import static testdata.TestData.DEFAULT_USER;
+import static testdata.TestData.INVALID_USER;
 
 public class ApiTests {
     UserController userController = new UserController();
@@ -99,6 +101,28 @@ public class ApiTests {
         //парсинг ответа в виде JSON на джава объект
         //передаем класс в котором нам нужно сделать сериализацию
         //после этого уже можем работать с джава объектом
+
+        Assertions.assertEquals(200, response.statusCode());
+        Assertions.assertEquals(200, createdUserResponse.getCode());
+        Assertions.assertEquals("unknown", createdUserResponse.getType());
+        Assertions.assertFalse(createdUserResponse.getMessage().isEmpty());
+    }
+
+    @Test
+    void createUserControllerTest2() {
+        Response response = userController.createUser(DEFAULT_USER);
+        AddUserResponse createdUserResponse = response.as(AddUserResponse.class);
+
+        Assertions.assertEquals(200, response.statusCode());
+        Assertions.assertEquals(200, createdUserResponse.getCode());
+        Assertions.assertEquals("unknown", createdUserResponse.getType());
+        Assertions.assertFalse(createdUserResponse.getMessage().isEmpty());
+    }
+
+    @Test
+    void createUserControllerTest3() {
+        Response response = userController.createUser(INVALID_USER);
+        AddUserResponse createdUserResponse = response.as(AddUserResponse.class);
 
         Assertions.assertEquals(200, response.statusCode());
         Assertions.assertEquals(200, createdUserResponse.getCode());
--- src/test/java/controllers/UserController.java
+++ src/test/java/controllers/UserController.java
@@ -3,13 +3,14 @@ package controllers;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import models.User;
+import static constants.CommonConstants.BASE_URL;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 
 
 public class UserController {
-    private static final String BASE_URI = "https://petstore.swagger.io/v2/";
+   // private static final String BASE_URL = "https://petstore.swagger.io/v2/";
     private static final String USER_ENDPOINT = "user";
     RequestSpecification requestSpecification;
 
@@ -17,7 +18,7 @@ public class UserController {
         this.requestSpecification = given()
                 .accept(JSON)
                 .contentType(JSON)
-                .baseUri(BASE_URI);
+                .baseUri(BASE_URL);
     }
 
     public Response createUser(User user) {
